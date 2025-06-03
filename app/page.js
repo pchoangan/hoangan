a4ad0861178315c7a9bac533d6350f2970543c49
Pham Cao Hoang An <pchoangan@gmail.com>
2025-06-03 13:40:57 +0700

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default async function HomePage() {
+  //const data = await getData(); // Fetch dữ liệu trên server
+  return (
+    <main>
+      <h1>Welcome to the Home Page</h1>
+      //<p>Data from server:{JSON.stringify(data)||"ok"} </p>
+    </main>
+  );
+}
