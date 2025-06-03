9dd38e00cc9dfe72f388d752f3f99493d73c151f
Pham Cao Hoang An <pchoangan@gmail.com>
2025-06-03 13:44:11 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,7 @@
-export default async function HomePage() {
-  //const data = await getData(); // Fetch dữ liệu trên server
+export default function HomePage() {  
   return (
     <main>
-      <h1>Welcome to the Home Page</h1>
-      //<p>Data from server:{JSON.stringify(data)||"ok"} </p>
+      <h1>Welcome to the Home Page</h1>      
     </main>
   );
 }
