4f7104e9bbd8b21c51a34b9b9336fd85b094cdb7
Pham Cao Hoang An <pchoangan@gmail.com>
2025-11-10 19:35:29 +0700

    Update homepage heading to 'Thẩm định KV5 - Ân'

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import "./table.css";
 export default function HomePage() {  
   return (
     <main>
-      <h1>Welcome to the Home Page</h1> 
+      <h1>Thẩm định KV5 - Ân</h1> 
       <Table />
     </main>
   );
