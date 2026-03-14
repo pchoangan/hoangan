27d4cc0db643cf1c9f17223111eadb168d80f7cf
Pham Cao Hoang An <pchoangan@gmail.com>
2026-03-14 22:07:18 +0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,7 @@
 "use client";
 import { useState, useEffect, useMemo } from "react";
 
-import { sendForm } from "./actions"; 
-import { fetchDataTable } from "./actions"; 
+
 
 
 export default function Table() {
